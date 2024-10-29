b9c0858d94032ebf11ce0abd2d2ebaea0449fb1c
rohit kumar <rohitk.techexactly@gmail.com>
2024-10-29 17:08:32 +0530

    i have update the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="futurapay",
-    version="0.2.4",
+    version="0.3.4",
     packages=find_packages(include=["Futurapay", "Futurapay.*"]),
     install_requires=['cryptography'],
     author="Rohit kumar",
